78718ad09fafef1eaec221afbb61352e8406ed2d
Yongfu <liao961120@gmail.com>
2021-09-16 18:49:22 +0800

    update

--- disqus-migration.R
+++ disqus-migration.R
@@ -1,6 +1,7 @@
 library(rvest)
 
 baseURL <- 'https://yongfu.name'
+baseURLold <- 'https://liao961120.github.io'
 
 posts <- read_html('https://yongfu.name/post/')
 
@@ -16,19 +17,17 @@ new_posts <- sapply(new_posts, function(x) {
   return(x)
 }, USE.NAMES = FALSE) %>% unlist()
 new_posts <- new_posts[!is.null(new_posts)]
-
-old_posts <- gsub("/$", ".html", new_posts) %>% paste0(baseURL, .)
-new_posts <- paste0(baseURL, new_posts)
+old_posts <- gsub("/$", ".html", new_posts)
 
 old_custom <- c('/2018/01/31/RlearningPath.html', 
     '/2019/03/04/OriginsOfHumanCommunication.html',
     '/2019/08/15/secretOfOurSuccess.html',
     '/2020/02/22/leipzigVue.html',
-    '/2017/12/11/Life_Tables.html') %>%
-  paste0(baseURL, .)
-new_custom <- tolower(old_custom) %>% gsub(".html", "/", ., fixed = T)
-old_posts <- c(old_posts, old_custom)
-new_posts <- c(new_posts, new_custom)
+    '/2017/12/11/Life_Tables.html')
+new_custom <- tolower(old_custom) %>% 
+  gsub(".html", "/", ., fixed = T)
+old_posts <- c(old_posts, old_custom) %>% paste0(baseURLold, .)
+new_posts <- c(new_posts, new_custom) %>% paste0(baseURL, .)
 
 csv <- paste(old_posts, new_posts, sep = ", ") 
 writeLines(paste0(csv, collapse = "\n"), "disqus.csv")
